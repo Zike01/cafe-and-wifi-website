24ec70943ea7a308fdfcf653a0e557ac644f390f
Zike01 <zikekomakoma@gmail.com>
2021-07-04 20:10:16 +0300

    update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 Bootstrap(app)
 load_dotenv()
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///cafes.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
